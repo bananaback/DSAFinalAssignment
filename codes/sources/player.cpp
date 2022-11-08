0b30e3a75ac75d09c0b003f6679173f2530b08a7
bananaback <votrongtin882003@gmail.com>
2022-11-09 00:01:14 +0700

    update player animation logic

--- codes/sources/player.cpp
+++ codes/sources/player.cpp
@@ -10,13 +10,18 @@ Player::Player(float x, float y, float width, float height, Game& game) :Unit(x,
 	_scaleY = 1;
 	_currentAnimation = 0;
 
-	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["1"], 0, 0, 16, 21, 1, (float)0.1, "idle"));
-	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["1_diagdown"], 0, 0, 20, 24, 4, (float)0.1, "1_diagdown"));
-	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["1_diagup"], 0, 0, 20, 24, 4, (float)0.1, "1_diagup"));
-	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["1_north"], 0, 0, 20, 24, 4, (float)0.1, "1_north"));
-	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["1_side"], 0, 0, 20, 24, 4, (float)0.1, "1_side"));
-	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["1_south"], 0, 0, 20, 24, 4, (float)0.1, "1_south"));
-	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["1_south2"], 0, 0, 20, 24, 4, (float)0.1, "1_south2"));
+	_monoFont.loadFromFile("assets/fonts/monogram-extended.ttf");
+	_playerDebugMessage.setFont(_monoFont);
+	_playerDebugMessage.setCharacterSize(24);
+	_playerDebugMessage.setFillColor(sf::Color::White);
+
+	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["e"], 0, 0, 20, 24, 4, (float)0.1, "e"));//0
+	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["ne"], 0, 0, 20, 24, 4, (float)0.1, "ne"));//1
+	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["se"], 0, 0, 20, 24, 4, (float)0.1, "se"));//2
+	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["n"], 0, 0, 20, 24, 4, (float)0.1, "n"));//3
+	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["s"], 0, 0, 20, 24, 4, (float)0.1, "s"));//4
+	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["sgun"], 0, 0, 20, 24, 4, (float)0.1, "sgun"));
+	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["idle"], 0, 0, 16, 21, 1, (float)0.1, "idle"));
 }
 
 Player::~Player() {
@@ -45,53 +50,15 @@ void Player::update(Game& game) {
         updateAnim = true;
     }
 
-
     if (updateAnim) _animations[_currentAnimation]->update(game._dt);
     sf::Vector2i mouse_pos = sf::Mouse::getPosition(game._window);
     float angle = std::atan2(-(mouse_pos.y - (_y + _width / 2)), -(mouse_pos.x - (_x + _height / 2))) * 180 / (std::atan(1) * 4);
-    if (angle >= 0) {
-        if (angle <= 22.5) {
-            _currentAnimation = 4;
-            _scaleX = -1;
-        }
-        else if (angle <= 22.5 + 45) {
-            _currentAnimation = 2;
-            _scaleX = -1;
-        }
-        else if (angle <= 90 + 22.5) {
-            _currentAnimation = 3;
-        }
-        else if (angle <= 135 + 22.5) {
-            _currentAnimation = 2;
-            _scaleX = 1;
-        }
-        else {
-            _currentAnimation = 4;
-            _scaleX = 1;
-        }
-    }
-    else if (angle < 0) {
-        if (angle >= -22.5) {
-            _currentAnimation = 4;
-            _scaleX = -1;
-        }
-        else if (angle >= -67.5) {
-            _currentAnimation = 1;
-            _scaleX = -1;
-        }
-        else if (angle >= -112.5) {
-            _currentAnimation = 5;
-        }
-        else if (angle >= -157.5) {
-            _currentAnimation = 1;
-            _scaleX = 1;
-        }
-        else {
-            _currentAnimation = 4;
-            _scaleX = 1;
-        }
-    }
-    //std::cout << game._dt << "\n";
+	int a = std::floor((angle + 180 + 22.5) / 45);
+	_currentAnimation = _assetName[a];
+	if (std::abs(angle) < 67.5) _scaleX = -1; else _scaleX = 1;
+
+	_playerDebugMessage.setPosition(_x, _y - 20);
+	_playerDebugMessage.setString(std::to_string(a));
 }
 
 void Player::draw(Game& game) {
@@ -105,4 +72,6 @@ void Player::draw(Game& game) {
 	rectangle.setSize(sf::Vector2f(_width, _height));
 	rectangle.setPosition(_x, _y);
 	rectangle.setFillColor(sf::Color(255, 255, 255, 100));
+
+	game._window.draw(_playerDebugMessage);
 }
