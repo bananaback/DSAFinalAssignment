2170d5cddbc6031eb1fe946d3fb8c1ad53161e78
bananaback <votrongtin882003@gmail.com>
2022-11-10 23:32:03 +0700

    remove magic number #2

--- codes/sources/player.cpp
+++ codes/sources/player.cpp
@@ -9,11 +9,13 @@ Player::Player(float x, float y, float width, float height, Game& game, std::sha
 	_width = width;
 	_height = height;
 	_name = "player";
-	_scaleX = 1;
-	_scaleY = 1;
 	_currentAnimation = 0;
 	_speed = 100;
 	_parentGrid = pGrid;
+	_scaleX = 3.f;
+	_scaleY = 3.f;
+	_assetWidth = 20;
+	_assetHeight = 24;
 
 	_monoFont.loadFromFile("assets/fonts/monogram-extended.ttf");
 	_playerDebugMessage.setFont(_monoFont);
@@ -21,13 +23,13 @@ Player::Player(float x, float y, float width, float height, Game& game, std::sha
 	_playerDebugMessage.setFillColor(sf::Color::White);
 
 	float frameDuration = 0.1f;
-	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["e"], 0, 0, 20, 24, 4, frameDuration, "e"));//0
-	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["ne"], 0, 0, 20, 24, 4, frameDuration, "ne"));//1
-	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["se"], 0, 0, 20, 24, 4, frameDuration, "se"));//2
-	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["n"], 0, 0, 20, 24, 4, frameDuration, "n"));//3
-	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["s"], 0, 0, 20, 24, 4, frameDuration, "s"));//4
-	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["sgun"], 0, 0, 20, 24, 4, frameDuration, "sgun"));
-	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["idle"], 0, 0, 20, 24, 1, frameDuration, "idle"));
+	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["e"], 0, 0, _assetWidth, _assetHeight, 4, frameDuration, "e"));//0
+	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["ne"], 0, 0, _assetWidth, _assetHeight, 4, frameDuration, "ne"));//1
+	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["se"], 0, 0, _assetWidth, _assetHeight, 4, frameDuration, "se"));//2
+	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["n"], 0, 0, _assetWidth, _assetHeight, 4, frameDuration, "n"));//3
+	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["s"], 0, 0, _assetWidth, _assetHeight, 4, frameDuration, "s"));//4
+	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["sgun"], 0, 0, _assetWidth, _assetHeight, 4, frameDuration, "sgun"));
+	_animations.push_back(std::make_shared<Animation>(game.ra_ptr->_imageResources["player"]["idle"], 0, 0, _assetWidth, _assetHeight, 1, frameDuration, "idle"));
 }
 
 Player::~Player() {
@@ -64,7 +66,7 @@ void Player::update(Game& game) {
     float angle = std::atan2(-(mouse_pos.y - (_y + _width / 2)), -(mouse_pos.x - (_x + _height / 2))) * 180 / (std::atan(1) * 4);
 	int a = std::floor((angle + 180 + 22.5) / 45);
 	_currentAnimation = _assetName[a];
-	if (std::abs(angle) < 67.5) _scaleX = -1; else _scaleX = 1;
+	if (std::abs(angle) < 67.5) _scaleX = -3.f; else _scaleX = 3.f;
 
 	_playerDebugMessage.setPosition(_x, _y - 20);
 	_playerDebugMessage.setString(std::to_string(a));
@@ -72,8 +74,8 @@ void Player::update(Game& game) {
 
 void Player::draw(Game& game) {
 	_animations[_currentAnimation]->_sprite.setPosition(_x + _width / 2, _y + _height / 2);
-	_animations[_currentAnimation]->_sprite.setOrigin(10, 12);
-	_animations[_currentAnimation]->_sprite.setScale(3.f * _scaleX, 3.f * _scaleY);
+	_animations[_currentAnimation]->_sprite.setOrigin(_assetWidth/2, _assetHeight/2);
+	_animations[_currentAnimation]->_sprite.setScale(_scaleX, _scaleY);
 	game._window.draw(_animations[_currentAnimation]->_sprite);
 
 
