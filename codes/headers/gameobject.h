3d4076ce07b3d3ca70b53e09f512773fca9d2419
bananaback <58079308+bananaback@users.noreply.github.com>
2022-11-23 19:13:59 +0700

    Update gameobject.h

--- codes/headers/gameobject.h
+++ codes/headers/gameobject.h
@@ -10,15 +10,9 @@ public:
 	void destroy();
 	float getX(), getY(), getWidth(), getHeight();
 	bool checkCollision(GameObject& other);
-	int getCollisionStatus(GameObject& other, int offset);
-	static const int COLLISION_LEFT = 1 << 0;
-	static const int COLLISION_RIGHT = 1 << 1;
-	static const int COLLISION_UP = 1 << 2;
-	static const int COLLISION_DOWN = 1 << 3;
 protected:
 	float _x, _y, _width, _height;
 	bool _isDestroyed;
-	
 };
 
 #endif // !GAMEOBJECT_H
